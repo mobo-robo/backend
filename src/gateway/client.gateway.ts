d6b4d08bb8dad6865010e970d84831314fd1a5c4
Akshay Ganesh <akshay.g@keyvalue.systems>
2023-09-16 00:15:17 +0530

    fix entity not found error

--- src/gateway/client.gateway.ts
+++ src/gateway/client.gateway.ts
@@ -58,6 +58,9 @@ export class ClientGateway implements OnGatewayConnection, OnGatewayDisconnect {
   async onData(@MessageBody() data, @ConnectedSocket() socket: any) {
     const { deviceId, secret } = this.getHeaders(socket);
     const devices = await this.deviceService.findDevicesToConnect(secret);
+    if (!devices) {
+      return;
+    }
     const device = devices.find(
       (device) => device.deviceId !== deviceId && device.isConnected
     );
