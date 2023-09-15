290e658df0fa9e1e07d5c4df8fd60fe6a7e120bd
Beena Vinod <beena@keyvalue.systems>
2023-09-15 21:22:42 +0530

    connect to client

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,6 +1,6 @@
 import { DeviceModule } from '@/device';
 import { LoggerModule } from '@/logger';
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { ClientGateway } from './client.gateway';
 
---
+++ src/gateway/guards/connection.guard.ts
@@ -0,0 +1,35 @@
+import { DEVICE_SERVICE } from "@/device/constants";
+import { DeviceService } from "@/device/services";
+import { Injectable, CanActivate, Inject } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class WsGuard implements CanActivate {
+
+    constructor(
+        @Inject(DEVICE_SERVICE)
+        private readonly deviceService: DeviceService) {
+    }
+
+    canActivate(
+        context: any,
+    ): boolean | any | Promise<boolean | any> | Observable<boolean | any> {
+        const deviceId = context.args[0].handshake.headers.deviceid;
+        const secret = context.args[0].handshake.headers.secret;
+        context.sample = 'abc';
+        try {
+            return new Promise((resolve, reject) => {
+                return this.deviceService.getDevice(deviceId,secret).then(user => {
+                    if (user) {
+                        resolve(user);
+                    } else {
+                        reject(false);
+                    }
+                });
+             });
+        } catch (ex) {
+            return false;
+        }
+        // return true;
+    }
+}
