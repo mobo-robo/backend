67b2a9d23265ae061d673e6a2c60d8246b9a2308
Lakshmi M S <lakshmi.m@keyvalue.systems>
2023-09-15 15:24:02 +0530

    fix: contacts module cleanup

--- src/contact/providers/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Provider } from '@nestjs/common';
-
-import { CONTACT_SERVICE } from '../constants';
-import { ContactService } from '../services';
-
-export const ContactServiceProvider: Provider = {
-  provide: CONTACT_SERVICE,
-  useClass: ContactService,
-};
